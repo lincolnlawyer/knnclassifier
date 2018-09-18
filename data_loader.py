58743aab3d49d0241c8d008417b3e8b116fce05b
Sreekar Yemmanoor <39303927+lincolnlawyer@users.noreply.github.com>
2018-09-18 22:38:44 +0530

    Fixed an error in the file path

--- data_loader.py
+++ data_loader.py
@@ -5,7 +5,7 @@ def unpickle(file):
 		d = pickle.load(fo, encoding='bytes')
 	return d 
 
-path = 'H:/learning-area/assignment1/cifar-10-batches-py'
+path = 'H:/learning-area/assignment1/cifar-10-batches-py/'
 files = ['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5']
 label_list = []
 data_array = np.zeros(1, 3072)
