1d704f225c292a8ee91f69b9f2894e433b067f7f
Sreekar Yemmanoor <39303927+lincolnlawyer@users.noreply.github.com>
2018-09-19 22:47:15 +0530

    Added code for loading test data as well

--- data_loader.py
+++ data_loader.py
@@ -6,14 +6,17 @@ def unpickle(file):
 	return d 
 
 path = 'H:/learning-area/assignment1/cifar-10-batches-py/'
-files = ['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5']
-label_list = []
+files = ['data_batch_1', 'data_batch_2', 'data_batch_3', 'data_batch_4', 'data_batch_5', 'test_batch']
+y_train = []
 data_array = np.zeros(1, 3072)
 for i in range(5):
 	temp = unpickle(path + files[i])
-	data_array = np.concatenate((data_arr, temp[b'data']), axis=0)
-	label_list += temp[b'labels']
-print(np.shape(data_array))
-print(len(label_list))
-train_data = data_array[1:]
+	data_array = np.concatenate((data_array, temp[b'data']), axis=0)
+	y_train += temp[b'labels']
+x_train = data_array[1:]
+print(np.shape(x_train))
+print(len(y_train))
 
+temp = unpickle(path + files[5])
+x_test = temp[b'data']
+y_test = temp[b'labels']
